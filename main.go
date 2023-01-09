526ee1b1ee74d7270c10dee4953483dc6226c8b9
Brandon Atkinson <opensource.atkin@gmail.com>
2023-01-09 10:24:13 -0700

    remove old testing code

--- main.go
+++ main.go
@@ -5,17 +5,11 @@ import (
 
 	"github.com/bjatkin/flappy_boot/gameplay"
 	"github.com/bjatkin/flappy_boot/internal/game"
-	"github.com/bjatkin/flappy_boot/internal/sprite"
 )
 
 //go:embed assets/gba
 var assets embed.FS
 
 func main() {
-	err := sprite.LoadPalette16(assets, "assets/gba/palette_0.p16", 0)
-	if err != nil {
-		return
-	}
-
 	game.Run(gameplay.NewDemo(assets))
 }
