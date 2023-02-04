3271ca2dd263575bd53b0729066f02f265667291
Brandon Atkinson <opensource.atkin@gmail.com>
2023-02-04 16:20:11 -0700

    get basic physics working

--- gameplay/demo.go
+++ gameplay/demo.go
@@ -12,16 +12,27 @@ import (
 	"github.com/bjatkin/flappy_boot/internal/mode0"
 )
 
+const (
+	// gravity influences how quickly the boot falls
+	gravity = 20
+
+	// maxDy is the terminal velocity for the boot
+	maxDy = 512
+
+	// grassY is the y level that the grass ground sits at
+	grassY = 144
+)
+
 // Demo is a test node used for prototyping basic mechanics
 type Demo struct {
 	assets embed.FS
-	tmp    uint
+	dy     int32
+	booty  int32
 }
 
 func NewDemo(assets embed.FS) *Demo {
 	return &Demo{
 		assets: assets,
-		tmp:    0xA,
 	}
 }
 
@@ -47,9 +58,10 @@ func (d *Demo) Init() error {
 	player := assets.NewPlayer()
 	player.Load()
 
+	// TODO: this should be managed through a sprite package
 	hw_sprite.OAM[0] = hw_sprite.Attrs{
-		Attr0: hw_sprite.Square | hw_sprite.Color16 | hw_sprite.Normal | 0xA,
-		Attr1: hw_sprite.Medium | hw_sprite.Attr1(d.tmp),
+		Attr0: hw_sprite.Square | hw_sprite.Color16 | hw_sprite.Normal | 72,
+		Attr1: hw_sprite.Medium | hw_sprite.Attr1(20),
 	}
 
 	mode0.Enable(
@@ -61,19 +73,26 @@ func (d *Demo) Init() error {
 }
 
 func (d *Demo) Update(frame uint) (game.Node, error) {
-	if key.PressedDown(key.Right) {
-		d.tmp += 256
+	if key.Pressed(key.A) {
+		d.dy -= 10
 	}
-	d.tmp += 64
-	d.tmp %= 256 << 8
+	if d.dy < maxDy {
+		d.dy += gravity
+	}
+
+	d.booty += d.dy
+
+	if d.booty > grassY<<8 {
+		d.booty = grassY << 8
+	}
 
 	return nil, nil
 }
 
 func (d *Demo) Draw() error {
 	hw_sprite.OAM[0] = hw_sprite.Attrs{
-		Attr0: hw_sprite.Square | hw_sprite.Color16 | hw_sprite.Normal | 0xA,
-		Attr1: hw_sprite.Medium | hw_sprite.Attr1(d.tmp>>8),
+		Attr0: hw_sprite.Square | hw_sprite.Color16 | hw_sprite.Normal | hw_sprite.Attr0(d.booty>>8),
+		Attr1: hw_sprite.Medium | 8,
 	}
 	return nil
 }
