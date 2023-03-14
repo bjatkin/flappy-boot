203f7a84852cc9886d81a341e024cc01eac8eba4
Brandon Atkinson <opensource.atkin@gmail.com>
2023-03-13 23:47:49 -0600

    feature: generate assets based on yaml config

---
+++ cmd/image_gen/internal/generate/templates.go
@@ -0,0 +1,48 @@
+package generate
+
+import (
+	"embed"
+	"html/template"
+	"strings"
+	"unicode"
+)
+
+//go:embed templates
+var templates embed.FS
+
+var goTemplates = template.Must(
+	template.New("go_templates").
+		Funcs(
+			map[string]any{
+				"private": private,
+				"public":  public,
+				"add":     add,
+			}).
+		ParseFS(templates, "templates/*.tmpl"),
+)
+
+func public(name string) string {
+	runes := []rune(name)
+	if unicode.IsLower(runes[0]) {
+		return strings.ToUpper(string(runes[0])) + string(runes[1:])
+	}
+
+	return name
+}
+
+func private(name string) string {
+	runes := []rune(name)
+	if unicode.IsUpper(runes[0]) {
+		return strings.ToLower(string(runes[0])) + string(runes[1:])
+	}
+
+	return name
+}
+
+func add(args ...int) int {
+	var total int
+	for _, a := range args {
+		total += a
+	}
+	return total
+}
